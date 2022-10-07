0549d6499babb46ac24b4f8bbcf4a352030c97f6
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-10-07 12:19:30 +0530

    base urls and stuff

--- superHeroGenerator/script.js
+++ superHeroGenerator/script.js
@@ -1,17 +1,16 @@
-const superHeroDiv = document.getElementById('superHero');
+const SUPERHERO_TOKEN = '10223569763528853'
+const BASE_URL = `https://superheroapi.com/api.php/${SUPERHERO_TOKEN}`
 
-console.log("run first");
-
-async function getSuperHero() {
-        const response = await fetch('https://superheroapi.com/api/2467543886757166/245',{
-                'mode' : 'no-cors'
-        });
-
-        return response.json;
-        // superHeroDiv.innerHTML = `<img src=${json.message} />`
+const getSuperHero = (id) => {
+        // name 👉 base_url/search/batman
+        // json.results[0].image.url
+        // id: 👉 base_url/id
+        // json.image.url
+        fetch(`${BASE_URL}/${id}`)
+                .then(response => response.json())
+                .then(json => {
+                        console.log(json)
+                        document.querySelectorAll('body').innerHTML += `<img src="${json.image.url}" height =200px width = 200px />`
+        })
 }
-
-getSuperHero()
-.then((json) => {
-        console.log(json);
-})
+getSuperHero(245);
