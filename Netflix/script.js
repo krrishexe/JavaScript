44b372364723dd695157db9c77c2ed3ffc4a616a
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-10-14 18:48:56 +0530

    update netflix script.js

--- Netflix/script.js
+++ Netflix/script.js
@@ -44,47 +44,60 @@ window.onload = () => {
 // ** Helper function that makes dynamic API calls **
 function fetchMovies(url, dom_element, path_type) {
     // Use Fetch with the url passed down 
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            showMovies(data, dom_element, path_type)
+        })
+        .catch(error => console.log(error));
 
-    // Within Fetch get the response and call showMovies() with the data , dom_element, and path type
+    // Within F etch get the response and call showMovies() with the data , dom_element, and path type
 }
+fetchMovies('https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213');
+
 
 //  ** Function that displays the movies to the DOM **
 showMovies = (movies, dom_element, path_type) => {
 
-    // Create a variable that grabs id or class
+    // Create a variable that grabs id or class                     when we have to select between id and class at the same time , then we have to use .queryselector()
 
+    var moviesEL = document.querySelector('dom_element');
 
-    // Loop through object
+    // Loop through object ( for of loop)
+    for (let movie of movies.results) {
+        // Within loop create an img element
+        let imageElement = document.createElement('img')
 
+        // Set attribute
+        imageElement.setAttribute('data-id', movie.id)
 
-    // Within loop create an img element
+        // Set source
+        imageElement.src = `https://image.tmdb.org/t/p/original${movie[path_type]}`
 
+        imageElement.addEventListener('click', e => {
+            handleMovieSelection(e)
+        })
 
-    // Set attribute
-
-
-    // Set source
-
-
-    // Add event listener to handleMovieSelection() onClick
-
-
-    // Append the imageElement to the dom_element selected
-
+        // Append the imageElement to the dom_element selected
+        moviesEL.appendChild(imageElement);                 // image element is the single single movies which we are adding to the original_movie div
+    }
 }
 
 
 // ** Function that fetches Netflix Originals **
 function getOriginals() {
-
+    let url = 'https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213'
+    fetchMovies(url, '.origianl__movies', 'poster_path')
 }
 // ** Function that fetches Trending Movies **
 function getTrendingNow() {
-
+    let url = 'https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045'
+    fetchMovies(url, '#trending', 'backdrop_path')
 }
 // ** Function that fetches Top Rated Movies **
 function getTopRated() {
-
+    let url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1'
+    fetchMovies(url,'#top_rated','backdrop_path');
 }
 
 // ** BONUS **
@@ -96,22 +109,22 @@ async function getMovieTrailer(id) {
 }
 
 // ** Function that adds movie data to the DOM
-const setTrailer = trailers => {
-    // Set up iframe variable to hold id of the movieTrailer Element
-    const iframe
-    // Set up variable to select .movieNotFound element
-    const movieNotFound
+// const setTrailer = trailers => {
+//     // Set up iframe variable to hold id of the movieTrailer Element
+//     const iframe
+//     // Set up variable to select .movieNotFound element
+//     const movieNotFound
 
-    // If there is a trailer add the src for it
-    if (trailers.length > 0) {
-        // add d-none class to movieNotFound and remove it from iframe
+//     // If there is a trailer add the src for it
+//     if (trailers.length > 0) {
+//         // add d-none class to movieNotFound and remove it from iframe
 
-        // add youtube link with trailers key to iframe.src
-    } else {
-        // Else remove d-none class to movieNotfound and ADD it to iframe
+//         // add youtube link with trailers key to iframe.src
+//     } else {
+//         // Else remove d-none class to movieNotfound and ADD it to iframe
 
-    }
-}
+//     }
+// }
 
 
 
